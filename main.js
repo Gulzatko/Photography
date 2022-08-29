adb2b07283711843a1e902e6a3c29ff009d0ca02
Gulzatko <Mamajanovna86@gmail.com>
2022-08-29 18:46:29 +0300

    created resposivness for the page, created animated scroll-down with js

---
+++ main.js
@@ -0,0 +1,10 @@
+//Animates Smooth Scroll
+$('#view-work').on('click',function(){
+    const images =$('#images').position().top;
+    $('html, body').animate(
+    {
+        scrollTop:images
+    },
+    2000
+    );
+});
